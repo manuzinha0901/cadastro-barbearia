9555657357f2bab00b036e8bf9aeb7726b18378a
manuzinha0901 <emanuelle.sanntoslima@gmail.com>
2024-11-11 13:18:10 -0300

    publicação

--- login.js
+++ login.js
@@ -1,23 +1,25 @@
-document.getElementById('loginForm').addEventListener('submit', async function(event) {
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", async function (event) {
     event.preventDefault();
 
-    const email = document.getElementById('email').value;
-    const senha = document.getElementById('senha').value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
 
-    const response = await fetch('https://appbarbearia.vercel.app/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, senha }),
+    const response = await fetch("https://barbeariadodi.vercel.app/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, senha }),
     });
 
-    const loginAlert = document.getElementById('loginAlert');
+    const loginAlert = document.getElementById("loginAlert");
     if (response.ok) {
-        window.location.href = 'home.html';
+      window.location.href = "home.html";
     } else {
-        loginAlert.textContent = 'Email ou senha incorretos.';
-        loginAlert.className = 'alert alert-danger';
-        loginAlert.style.display = 'block';
+      loginAlert.textContent = "Email ou senha incorretos.";
+      loginAlert.className = "alert alert-danger";
+      loginAlert.style.display = "block";
     }
-});
+  });
